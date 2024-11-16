cdd74817a5d9f72e2aa16695893275a3a72b288b
WJG <wjg12341234@163.com>
2024-11-16 11:11:41 +0800

    chore: remove nodejs zip script

--- scripts/build-server.sh
+++ scripts/build-server.sh
@@ -1,7 +1,7 @@
-# Build Server
-python -m nuitka --standalone \
+python -m nuitka --standalone --assume-yes-for-downloads \
   --include-data-files="src-python/models/*.onnx=models/" \
   --output-dir=out src-python/server.py
 
-# Zip Files
-node scripts/zip-server.js
+cd out/server.dist && zip -r ../server.zip .
+
+echo "✅ Done"
